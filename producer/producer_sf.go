6e274994bc6951a2564e2fa0c4cf10d15ebb321c
Simon Podlipsky <simon@podlipsky.net>
2021-06-08 08:27:55 +0200

    Add BGP Next Hop field to flow message

--- producer/producer_sf.go
+++ producer/producer_sf.go
@@ -266,7 +266,7 @@ func SearchSFlowSamplesConfig(samples []interface{}, config *SFlowProducerConfig
 				flowMessage.DstNet = recordData.DstMaskLen
 			case sflow.ExtendedGateway:
 				ipNh = recordData.NextHop
-				flowMessage.NextHop = ipNh
+				flowMessage.BgpNextHop = ipNh
 				flowMessage.SrcAS = recordData.SrcAS
 				if len(recordData.ASPath) > 0 {
 					flowMessage.DstAS = recordData.ASPath[len(recordData.ASPath)-1]
