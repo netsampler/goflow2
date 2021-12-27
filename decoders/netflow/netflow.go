8114e2e79e73d89fc009bfcfe642cebfeb1814cb
lspgn <lspgn@users.noreply.github.com>
2021-12-27 13:16:39 +0100

    add file driver and store templates as json

--- decoders/netflow/netflow.go
+++ decoders/netflow/netflow.go
@@ -247,7 +247,7 @@ func DecodeMessage(payload *bytes.Buffer, templates templates.TemplateInterface)
 	return DecodeMessageContext(context.Background(), payload, "", templates)
 }
 
-func DecodeMessageContext(ctx context.Context, payload *bytes.Buffer, templateKey string, templates templates.TemplateInterface) (interface{}, error) {
+func DecodeMessageContext(ctx context.Context, payload *bytes.Buffer, templateKey string, tpli templates.TemplateInterface) (interface{}, error) {
 	var size uint16
 	packetNFv9 := NFv9Packet{}
 	packetIPFIX := IPFIXPacket{}
@@ -307,9 +307,9 @@ func DecodeMessageContext(ctx context.Context, payload *bytes.Buffer, templateKe
 
 			flowSet = templatefs
 
-			if templates != nil {
+			if tpli != nil {
 				for _, record := range records {
-					templates.AddTemplate(ctx, templateKey, version, obsDomainId, record.TemplateId, record)
+					tpli.AddTemplate(ctx, templates.NewTemplateKey(templateKey, version, obsDomainId, record.TemplateId), record)
 				}
 			}
 
@@ -325,9 +325,9 @@ func DecodeMessageContext(ctx context.Context, payload *bytes.Buffer, templateKe
 			}
 			flowSet = optsTemplatefs
 
-			if templates != nil {
+			if tpli != nil {
 				for _, record := range records {
-					templates.AddTemplate(ctx, templateKey, version, obsDomainId, record.TemplateId, record)
+					tpli.AddTemplate(ctx, templates.NewTemplateKey(templateKey, version, obsDomainId, record.TemplateId), record)
 				}
 			}
 
@@ -343,9 +343,9 @@ func DecodeMessageContext(ctx context.Context, payload *bytes.Buffer, templateKe
 			}
 			flowSet = templatefs
 
-			if templates != nil {
+			if tpli != nil {
 				for _, record := range records {
-					templates.AddTemplate(ctx, templateKey, version, obsDomainId, record.TemplateId, record)
+					tpli.AddTemplate(ctx, templates.NewTemplateKey(templateKey, version, obsDomainId, record.TemplateId), record)
 				}
 			}
 
@@ -361,20 +361,20 @@ func DecodeMessageContext(ctx context.Context, payload *bytes.Buffer, templateKe
 			}
 			flowSet = optsTemplatefs
 
-			if templates != nil {
+			if tpli != nil {
 				for _, record := range records {
-					templates.AddTemplate(ctx, templateKey, version, obsDomainId, record.TemplateId, record)
+					tpli.AddTemplate(ctx, templates.NewTemplateKey(templateKey, version, obsDomainId, record.TemplateId), record)
 				}
 			}
 
 		} else if fsheader.Id >= 256 {
 			dataReader := bytes.NewBuffer(payload.Next(nextrelpos))
 
-			if templates == nil {
+			if tpli == nil {
 				continue
 			}
 
-			template, err := templates.GetTemplate(ctx, templateKey, version, obsDomainId, fsheader.Id)
+			template, err := tpli.GetTemplate(ctx, templates.NewTemplateKey(templateKey, version, obsDomainId, fsheader.Id))
 
 			if err == nil {
 				switch templatec := template.(type) {
--- decoders/netflow/templates/templates.go
+++ decoders/netflow/templates/templates.go
@@ -3,6 +3,8 @@ package templates
 import (
 	"context"
 	"fmt"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -19,22 +21,83 @@ type TemplateDriver interface {
 	Close(context.Context) error // Close drive (eg: close file)
 }
 
+type TemplateKey struct {
+	TemplateKey string
+	Version     uint16
+	ObsDomainId uint32
+	TemplateId  uint16
+}
+
+func NewTemplateKey(templateKey string, version uint16, obsDomainId uint32, templateId uint16) *TemplateKey {
+	return &TemplateKey{
+		TemplateKey: templateKey,
+		Version:     version,
+		ObsDomainId: obsDomainId,
+		TemplateId:  templateId,
+	}
+}
+
+func (k *TemplateKey) String() string {
+	return fmt.Sprintf("%s-%d-%d-%d", k.TemplateKey, k.Version, k.ObsDomainId, k.TemplateId)
+}
+
+func ParseTemplateKey(key string, k *TemplateKey) error {
+	if k != nil {
+		return nil
+	}
+	var version uint16
+	var obsDomainId uint32
+	var templateId uint16
+
+	keySplit := strings.Split(key, "-")
+	if len(keySplit) != 4 {
+		return fmt.Errorf("template key format is invalid")
+	}
+	templateKey := keySplit[0]
+	if val, err := strconv.ParseUint(keySplit[1], 10, 64); err != nil {
+		return fmt.Errorf("template key version is invalid")
+	} else {
+		version = uint16(val)
+	}
+	if val, err := strconv.ParseUint(keySplit[2], 10, 64); err != nil {
+		fmt.Errorf("template key observation domain I Dis invalid")
+	} else {
+		obsDomainId = uint32(val)
+	}
+	if val, err := strconv.ParseUint(keySplit[3], 10, 64); err != nil {
+		fmt.Errorf("template key template ID is invalid")
+	} else {
+		templateId = uint16(val)
+	}
+
+	k.TemplateKey = templateKey
+	k.Version = version
+	k.ObsDomainId = obsDomainId
+	k.TemplateId = templateId
+
+	return nil
+}
+
 type TemplateInterface interface {
-	//ListTemplate(ctx context.Context) error // to complete
-	GetTemplate(ctx context.Context, templateKey string, version uint16, obsDomainId uint32, templateId uint16) (interface{}, error)
-	AddTemplate(ctx context.Context, templateKey string, version uint16, obsDomainId uint32, templateId uint16, template interface{}) error // add expiration
+	ListTemplates(ctx context.Context, ch chan *TemplateKey) error
+	GetTemplate(ctx context.Context, key *TemplateKey) (interface{}, error)
+	AddTemplate(ctx context.Context, key *TemplateKey, template interface{}) error // add expiration
 }
 
 type TemplateSystem struct {
 	driver TemplateDriver
 }
 
-func (t *TemplateSystem) AddTemplate(ctx context.Context, templateKey string, version uint16, obsDomainId uint32, templateId uint16, template interface{}) error {
-	return t.driver.AddTemplate(ctx, templateKey, version, obsDomainId, templateId, template)
+func (t *TemplateSystem) ListTemplates(ctx context.Context, ch chan *TemplateKey) error {
+	return t.driver.ListTemplates(ctx, ch)
 }
 
-func (t *TemplateSystem) GetTemplate(ctx context.Context, templateKey string, version uint16, obsDomainId uint32, templateId uint16) (interface{}, error) {
-	return t.driver.GetTemplate(ctx, templateKey, version, obsDomainId, templateId)
+func (t *TemplateSystem) AddTemplate(ctx context.Context, key *TemplateKey, template interface{}) error {
+	return t.driver.AddTemplate(ctx, key, template)
+}
+
+func (t *TemplateSystem) GetTemplate(ctx context.Context, key *TemplateKey) (interface{}, error) {
+	return t.driver.GetTemplate(ctx, key)
 }
 
 func (t *TemplateSystem) Close(ctx context.Context) error {
