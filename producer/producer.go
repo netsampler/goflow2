854ce97bd0badb2cd016cbef2f8a7f972f0d5105
lspgn <lspgn@users.noreply.github.com>
2023-04-15 16:11:35 -0700

    nit

--- producer/producer.go
+++ producer/producer.go
@@ -1,9 +1,7 @@
 package producer
 
 import (
-	"fmt"
 	"net/netip"
-	"sync"
 	"time"
 )
 
@@ -23,44 +21,4 @@ type ProduceArgs struct {
 	Dst            netip.AddrPort
 	SamplerAddress netip.Addr
 	TimeReceived   time.Time
-}
-
-var (
-	producers = make(map[string]ProducerInterface)
-	lock      = &sync.RWMutex{}
-)
-
-func RegisterProducer(name string, t ProducerInterface) {
-	lock.Lock()
-	producers[name] = t
-	lock.Unlock()
-
-	/*if err := t.Prepare(); err != nil {
-		panic(err) // ?improve?
-	}*/
-}
-
-func FindProducer(name string) (ProducerInterface, error) {
-	lock.RLock()
-	t, ok := producers[name]
-	lock.RUnlock()
-	if !ok {
-		return nil, fmt.Errorf("Producer %s not found", name)
-	}
-
-	/*err := t.Init()
-	return t, err*/
-	return t, nil
-}
-
-func GetProducers() []string {
-	lock.RLock()
-	defer lock.RUnlock()
-	t := make([]string, len(producers))
-	var i int
-	for k, _ := range producers {
-		t[i] = k
-		i++
-	}
-	return t
 }
